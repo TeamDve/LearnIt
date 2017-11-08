6d6d442e5a11e1dd6c23e6f212f84339abcc8487
pavelYankov <prazor@abv.bg>
2017-11-08 10:11:10 +0200

    changed the layout to what it would sorta look like

---
+++ LearnIt/LearnIt/Areas/Projects/Controllers/ProjectsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnIt.Areas.Projects.Controllers
+{
+    public class ProjectsController : Controller
+    {
+
+        public ActionResult AllProjects()
+        {
+            return this.View();
+        }
+
+        public ActionResult MyProjects()
+        {
+            return this.View();
+        }
+    }
+}
