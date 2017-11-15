c8246c8b6fcd49ac90b095f685ac822fdba1ca3d
Stanislav <stanislavbabalev96@gmail.com>
2017-11-15 13:39:15 +0200

    Unit test

--- LearnIt/LearnIt.Tests/Services/DataServices/CourseServiceTests/GetAllCourses_Should.cs
+++ LearnIt/LearnIt.Tests/Services/DataServices/CourseServiceTests/GetAllCourses_Should.cs
@@ -1,14 +1,14 @@
-﻿//using LearnIt.Data.Context;
-//using LearnIt.Data.Models;
-//using LearnIt.Data.Services;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Moq;
-//using System;
-//using System.Collections.Generic;
-//using System.Data.Entity;
-//using System.Linq;
+﻿using LearnIt.Data.Context;
+using LearnIt.Data.Models;
+using LearnIt.Data.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 namespace LearnIt.Tests.Services.DataServices.CourseServiceTests
 {
     [TestClass]
@@ -43,60 +43,21 @@ namespace LearnIt.Tests.Services.DataServices.CourseServiceTests
             Assert.AreEqual(valueToAssertAgainst.Name, testedObject.Name);
             Assert.AreEqual(valueToAssertAgainst.ScoreToPass, testedObject.ScoreToPass);
         }
-=======
->>>>>>> 4e9bd532d9d935b206e57f9e66482e47b1e637cf
-
-//namespace LearnIt.Tests.Services.DataServices.CourseServiceTests
-//{
-//    [TestClass]
-//    public class GetAllCourses_Should
-//    {
-//        [TestMethod]
-//        public void ReturnListOfCourses_WhenTheyArePresent()
-//        {
-//            //Arrange
-//            var dateTime = DateTime.Now;
-//            var dbContextMock = new Mock<ApplicationDbContext>();
-//            List<Course> courseList = new List<Course>()
-//            {
-//                new Course()
-//                {
-//                    Name = "eklerEkler",
-//                    Description = "NeshtoSiNeshtoSi",
-//                    DateAdded = dateTime,
-//                    Required = true,
-//                    ScoreToPass = 40
-//                }
-//            };
-//            var coursesMock = new Mock<DbSet<Course>>().SetupData(courseList);
-//            dbContextMock.SetupGet(x => x.Courses).Returns(coursesMock.Object);
-//            //Act
-//            CourseService courseService = new CourseService(dbContextMock.Object);
-//            var valueToAssertAgainst = courseList.Single();
-//            var testedObject = courseService.GetAllCourses().Single();
-//            //Assert
-//            Assert.AreEqual(valueToAssertAgainst.DateAdded, testedObject.DateAdded);
-//            Assert.AreEqual(valueToAssertAgainst.Description, testedObject.Description);
-//            Assert.AreEqual(valueToAssertAgainst.Name, testedObject.Name);
-//            Assert.AreEqual(valueToAssertAgainst.Required, testedObject.Required);
-//            Assert.AreEqual(valueToAssertAgainst.ScoreToPass, testedObject.ScoreToPass);
-//        }
-
-//        [TestMethod]
-//        public void ReturnEmptyListOfCourses_WhenNoneArePresent()
-//        {
-//            //Arrange
-//            var dateTime = DateTime.Now;
-//            var dbContextMock = new Mock<ApplicationDbContext>();
-//            List<Course> courseList = new List<Course>()
-//            {
-//            };
-//            var coursesMock = new Mock<DbSet<Course>>().SetupData(courseList);
-//            dbContextMock.SetupGet(x => x.Courses).Returns(coursesMock.Object);
-//            //Act
-//            CourseService courseService = new CourseService(dbContextMock.Object);
-//            //Assert
-//            Assert.AreNotEqual(courseList, courseService.GetAllCourses());
-//        }
-//    }
-//}
+        [TestMethod]
+        public void ReturnEmptyListOfCourses_WhenNoneArePresent()
+        {
+            //Arrange
+            var dateTime = DateTime.Now;
+            var dbContextMock = new Mock<ApplicationDbContext>();
+            List<Course> courseList = new List<Course>()
+            {
+            };
+            var coursesMock = new Mock<DbSet<Course>>().SetupData(courseList);
+            dbContextMock.SetupGet(x => x.Courses).Returns(coursesMock.Object);
+            //Act
+            CourseService courseService = new CourseService(dbContextMock.Object);
+            //Assert
+            Assert.AreNotEqual(courseList, courseService.GetAllCourses());
+        }
+    }
+}
