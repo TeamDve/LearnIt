950ff4cf55eb7b55f46fbd0857ee616b24be5bd9
MartinIvanov90 <martin.g.ivanov@gmail.com>
2017-11-15 17:21:53 +0200

    just in case

---
+++ LearnIt/LearnIt.Tests/Web/Controllers/Areas/Courses/Controllers/CourseControllerTests/Constructor_Should.cs
@@ -0,0 +1,27 @@
+﻿using LearnIt.Areas.Courses.Controllers;
+using LearnIt.Data.Services.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnIt.Tests.Web.Controllers.Areas.Courses.Controllers.CourseControllerTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void InitializeController_WhenProperArgumentsAreGiven()
+        {
+            //Arrange
+            var courseService = new Mock<ICourseService>();
+            //Act
+            var controller = new CoursesController(courseService.Object);
+            //Assert
+            Assert.IsNotNull(controller);
+        }
+    }
+}
